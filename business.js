f620450e16727e0413b15d4b77f680ba225bcf02
klotyks <208618052+klotyks@users.noreply.github.com>
2026-07-05 00:08:57 +0200

    Added: class(dropStuffById) and improved class(Market)

--- business.js
+++ business.js
@@ -11,11 +11,12 @@ class Npc {
     this.stuffs.push(stuff)
   }
 
-  // dropStaffById(stuffId) {
-  //   const stuff = this.stuffs.find(s => s.id === stuffId)
-  //   this.stuffs = this.stuffs.filter(s => s.id !== stuffId)
-  //   return stuff
-  // }
+  dropStuffById(stuffId) {
+    const index = this.stuffs.findIndex(s => s.id === stuffId)
+    if (index === -1) return null
+
+    return this.stuffs.splice(index, 1)[0]
+  }
 }
 
 class Stuff {
@@ -27,26 +28,36 @@ class Stuff {
 }
 
 class Market {
-  constructor() {
+  constructor(npcs) {
+    this.npcs = npcs
     this.offers = []
-    this.lots = []
     this.offersLimit = 9
   }
 
   addOffer(ownerId, stuffId, price) {
-    const offer = { id: randId(), ownerId, price, stuffId }
+    const offer = { id: randId(), ownerId, stuffId, price }
     this.offers.push(offer)
   }
 
-  getMarketVolume() {
-    return this.offers.reduce((acc, item) => acc + o.price, 0)
-  }
-
   dealByOfferId(offerId, buyer) {
-    const offer = this.offers.find(o => o.id === offerId)
-    if (!offer) return
-    if (buyer.money >= offer.price) {
-      buyer.money -= offer.price
-    }
+    const offerIndex = this.offers.findIndex(o => o.id === offerId)
+    if (offerIndex === -1) return false
+    const offer = this.offers[offerIndex]
+    if (buyer.money < offer.price) return false
+
+    const seller = this.npcs.find(n => n.id === offer.ownerId)
+    if (!seller) return false
+
+    const stuff = seller.dropStuffById(offer.stuffId)
+    if (!stuff) return false
+
+    buyer.money -= offer.price
+    seller.money += offer.price
+
+    buyer.addStuff(stuff)
+
+    this.offers.splice(offerIndex, 1)
+
+    return true
   }
 }
